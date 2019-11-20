bcfa1bfa6e1895d4c848cd07800331540b034627
ahmad sofyan <sofyanahmad2727@gmail.com>
2019-11-20 19:32:23 +0700

    first commit

---
+++ tutor1/tutor1/app/src/main/java/com/example/tutor1/aplikasi1.java
@@ -0,0 +1,14 @@
+package com.example.tutor1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class aplikasi1 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coba1);
+    }
+}
